c32163f6f236e1063cf194f4384717da34b2cdf1
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2024-12-11 12:48:15 +0530

    Pending(API not working)

---
+++ Quote of the Day/script.js
@@ -0,0 +1,9 @@
+const api_url = "https://zenquotes.io/api/random";
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
+getQuote(api_url);
