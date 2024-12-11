bc7ddccd2bd227b37d95aefabf80c205a7778b9d
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2024-12-11 08:58:48 +0530

    Notes Keep App

---
+++ Notes Keep/script.js
@@ -0,0 +1,37 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || ""; // Avoid setting null to innerHTML
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "image/bin.png";
+    inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox);
+    updateStorage(); // Ensure storage is updated immediately
+});
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box"); // Update notes NodeList
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            };
+        });
+    }
+});
