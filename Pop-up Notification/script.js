38c92f38a4e0f0a436a1ed5c9fcef6db97c8af13
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2024-12-12 11:17:32 +0530

    Pop-up Notification

---
+++ Pop-up Notification/script.js
@@ -0,0 +1,8 @@
+let popup=document.getElementById("popup");
+
+function showpopup() {
+    popup.classList.add("open-popup");
+}
+function closepopup() {
+    popup.classList.remove("open-popup");
+}
